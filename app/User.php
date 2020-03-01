8cc7fa0c2924a32c0f2d2631df8bbca647d56d00
EDWARDO BARLIAN <edwardo.barlian@binus.ac.id>
2020-03-01 12:10:01 +0700

    add roles to user

--- app/User.php
+++ app/User.php
@@ -35,5 +35,7 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'birthday' => 'datetime',
+        'roles' => 'array'
     ];
 }
